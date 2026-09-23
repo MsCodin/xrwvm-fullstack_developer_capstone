39161e3d015178cb4184ab3ba826e154f3c50044
Mihaela Staneva <mihaelastaneva06@gmail.com>
2026-09-23 16:27:51 +0100

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,5 +1,5 @@
 # Uncomment the imports below before you add the function code
-# import requests
+import requests
 import os
 from dotenv import load_dotenv
 
@@ -11,12 +11,46 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
-# def get_request(endpoint, **kwargs):
-# Add code for get requests to back end
 
-# def analyze_review_sentiments(text):
-# request_url = sentiment_analyzer_url+"analyze/"+text
-# Add code for retrieving sentiments
+def get_request(endpoint, **kwargs):
+    params = ""
+    if kwargs:
+        for key, value in kwargs.items():
+            params = params + key + "=" + str(value) + "&"
+    
+    request_url = backend_url + endpoint + "?" + params
+    print("GET from {} ".format(request_url))
+    
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(request_url)
+        return response.json()
+    except Exception as e:
+        # If any error occurs
+        print(f"Network exception occurred: {e}")
+        return None
 
-# def post_review(data_dict):
-# Add code for posting review
+
+def analyze_review_sentiments(text):
+    request_url = sentiment_analyzer_url + "analyze/" + text
+    print("GET from {} ".format(request_url))
+    
+    try:
+        response = requests.get(request_url)
+        return response.json()
+    except Exception as e:
+        print(f"Network exception occurred: {e}")
+        return None
+
+
+def post_review(data_dict):
+    request_url = backend_url + "/insert_review"
+    print("POST to {} ".format(request_url))
+    
+    try:
+        response = requests.post(request_url, json=data_dict)
+        print(response.status_code)
+        return response.json()
+    except Exception as e:
+        print(f"Network exception occurred: {e}")
+        return None
