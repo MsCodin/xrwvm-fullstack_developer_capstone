57dd1bfa3c9f47a4ca8f88b8ebffd8c56657adbd
Mihaela Staneva <mihaelastaneva06@gmail.com>
2026-09-23 14:47:32 +0100

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,4 +1,3 @@
-# Uncomment the imports before you add the code
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
@@ -7,13 +6,7 @@ from . import views
 app_name = 'djangoapp'
 
 urlpatterns = [
-    # path for registration
     path(route='registration', view=views.registration, name='registration'),
-    
-    # path for login
     path(route='login', view=views.login_user, name='login'),
-    
-    # path for logout
     path(route='logout', view=views.logout_request, name='logout'),
-    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
